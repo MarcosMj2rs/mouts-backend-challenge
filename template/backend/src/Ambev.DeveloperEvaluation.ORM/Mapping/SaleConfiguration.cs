c8ffb84879a3698db582259fd7249a9a7d623990
Marcos <marcos.jesus@cwi.com.br>
2026-06-26 04:15:47 -0300

    feat: add sales entity mappings

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.ToTable("Sales");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.SaleNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(s => s.SaleDate)
+                .IsRequired();
+
+            builder.Property(s => s.CustomerId)
+                .IsRequired();
+
+            builder.Property(s => s.CustomerName)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(s => s.BranchId)
+                .IsRequired();
+
+            builder.Property(s => s.BranchName)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(s => s.TotalAmount)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(s => s.IsCancelled)
+                .IsRequired();
+
+            builder.Property(s => s.CreatedAt)
+                .IsRequired();
+
+            builder.Property(s => s.UpdatedAt);
+
+            builder.HasMany(s => s.Items)
+                .WithOne()
+                .HasForeignKey(i => i.SaleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable("SaleItems");
+
+            builder.HasKey(i => i.Id);
+
+            builder.Property(i => i.SaleId)
+                .IsRequired();
+
+            builder.Property(i => i.ProductId)
+                .IsRequired();
+
+            builder.Property(i => i.ProductDescription)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(i => i.Quantity)
+                .IsRequired();
+
+            builder.Property(i => i.UnitPrice)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(i => i.Subtotal)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(i => i.DiscountAmount)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(i => i.TotalAmount)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(i => i.IsCancelled)
+                .IsRequired();
+
+            builder.Property(i => i.CreatedAt)
+                .IsRequired();
+
+            builder.Property(i => i.UpdatedAt);
+        }
+    }
+}
